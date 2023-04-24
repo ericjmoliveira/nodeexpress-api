219fa95fe1cce57f8b40b2861d2d7b852ac9c23f
Eric Oliveira <ericjmoliveira@gmail.com>
2023-04-24 09:21:54 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 
+import { playerRouter } from './routes/player';
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Hello world!' }));
+app.use(express.json());
+app.use(playerRouter);
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
